19c13526311fc41d0bf100a8428b2627bbcfbd28
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-02 21:14:54 +0530

    practice

---
+++ DemoIfElse.py
@@ -0,0 +1,31 @@
+# if else loop in python
+# x = 10
+
+# if x > 5:
+#     print("x is greater than 5")
+# else:
+#     print("x is not greater than 5")
+
+
+# if elif else 
+
+# score = 85
+# if(score >= 90):
+#     print("Grade: A")
+# elif score >= 75:
+#     print("Grade: B")
+# elif score >= 60:
+#     print("Grade: C")
+# else:
+#     print("Grade: D")
+
+# nested if else loop
+num = 12
+
+if num % 2 == 0:
+    if num > 10:
+        print("Even number greater than 10")
+    else:
+        print("Even number is less than or equal to 10")
+else:
+    print("Odd number")
