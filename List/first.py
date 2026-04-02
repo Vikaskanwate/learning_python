714c596d572ce3b62f716f15e4bf950a9831d0b6
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-02 23:12:33 +0530

    practice

--- List/first.py
+++ List/first.py
@@ -1,2 +1,16 @@
-marks = [1,2,3]
-print(marks) 
+# fruits = ["apple","banana","cherry"]
+
+# fruits.append("orange")
+# fruits.insert(1,"grape")
+# fruits.remove("banana")
+# fruits.pop(2)
+# del fruits[0]
+# print(fruits)
+
+# slicing in python
+
+numbers = [10,20,30,40,50]
+print(numbers[1:4:1])
+print(numbers[-5:3:1])
+# it's not possible the go in backword direction once direction is decided based on the step value which is last one
+print(numbers[4:-1:-1])
