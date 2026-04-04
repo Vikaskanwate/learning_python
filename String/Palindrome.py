6abc89733453466de0d0229a6e7893a1c02073a5
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-05 00:18:36 +0530

    practice

--- String/Palindrome.py
+++ String/Palindrome.py
@@ -1,5 +1,15 @@
+# word = "madam"
+# if word == word[::-1]:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
 word = "madam"
-if word == word[::-1]:
+
+name = "".join(reversed(word))
+
+if word == name:
+    print()
     print("palindrome")
 else:
     print("not palindrome")
