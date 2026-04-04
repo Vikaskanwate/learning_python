3864cac8264e31f8061e4c6178983e22d24b4a03
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-04 19:43:00 +0530

    practice

--- String/RemoveDuplicate.py
+++ String/RemoveDuplicate.py
@@ -1,7 +1,11 @@
-text = "programming"
-result = ""
+# text = "programming"
+# result = ""
 
-for char in text:
-    if char not in result:
-        result += char
+# for char in text:
+#     if char not in result:
+#         result += char
+# print(result)
+
+s = "pogramming"
+result = "".join(dict.fromkeys(s))
 print(result)
--- String/Rev.py
+++ String/Rev.py
@@ -1,3 +1,11 @@
-text = "Hello World"
-reversed_text = text[::-1]
-print(reversed_text)
+# text = "Hello World"
+# reversed_text = text[::-1]
+# print(reversed_text)
+
+# s = 'hello'
+# for ch in reversed(s):
+#     print(ch, end="")
+
+
+s = 'hello'
+print("".join(reversed(s)))
