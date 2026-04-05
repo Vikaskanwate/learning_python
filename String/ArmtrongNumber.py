54e53ea670c57d0a7ce2a75d1a610ec9b23d4d79
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-05 21:55:14 +0530

    practice

--- String/ArmtrongNumber.py
+++ String/ArmtrongNumber.py
@@ -1,1 +1,15 @@
-num = 153
+num = "9475"
+n = len(num)
+number = int(num)
+temp = number
+sum = 0
+
+while (number > 0):
+    digit = number % 10
+    sum += (digit ** n)
+    number //= 10
+
+if sum == temp:
+    print("number is armstrong")
+else:
+    print("number is not armstrong")
