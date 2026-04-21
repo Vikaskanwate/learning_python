ca41633171245d461517114281c35f58fb42900c
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-21 19:40:48 +0530

    practicing numbers

---
+++ List/Test_8.py
@@ -0,0 +1,12 @@
+# prime number 
+n = int(input('Enter the number\n'))
+def is_prime(n):
+    for i in range(2,n//2+1):
+        if n % i == 0:
+            return False
+    return True 
+
+if is_prime(n):
+    print("Number is prime")
+else:
+    print("Number is not Prime")
