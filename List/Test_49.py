5378a921bb77b2cf39f5c296bb19d5f59cdc4f5c
Vikaskanwate <vikaskanwate32@gmail.com>
2026-05-20 20:06:21 +0530

    practice

--- List/Test_49.py
+++ List/Test_49.py
@@ -1,6 +1,11 @@
 # remove duplicate element
 list1 = [1, 2, 3, 4, 5, 2, 3]
-list2 = [3, 4,7, 4, 3]
+list2 = [3, 4, 4, 3]
 
 s = list(set(list1))
 print(s)
+
+if all(i in list1 for i in list2):
+    print("list2 is subset of list1")
+else:
+    print("list2 is not subset of list1")
